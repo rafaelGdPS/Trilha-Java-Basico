fd7977da11fef170b2b848903d36cc9dfcd32baf
Rafael <rafaelgermanio@gmail.com>
2024-03-27 19:06:53 -0300

    Feat: Buscando do terminal valores pras variaveis e exibindo  no  terminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+
+    System.out.println("Por favor, digite seu nome");
+    String nomeCliente = scanner.next();
+    System.out.println("Por favor digite o numero da sua conta");
+    int numero = scanner.nextInt();
+
+    System.out.println("Por favor, digite o número da Agência !");
+    String agencia =  scanner.next();
+
+    System.out.println("Por favor, digite o saldo");
+    double saldo = scanner.nextDouble();
+
+    
+
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo " + saldo + " já está disponível para saque");
+
+    scanner.close();
+  }
+
+}
